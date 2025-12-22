b29d8178fa080aad91089ae3330f3bc87aa2587a
nithishvaduganathan <nithishvaduganathan07@gmail.com>
2025-12-22 07:45:46 +0530

    sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,18 @@
+export default function sitemap() {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
+
+  return [
+    {
+      url: ${baseUrl},
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1.0,
+    },
+    {
+      url: ${baseUrl}/blog,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
+}
