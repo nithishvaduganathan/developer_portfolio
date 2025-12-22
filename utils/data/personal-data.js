49b45853237f5336d1ff2e5bb60d5d7028039a6d
nithishvaduganathan <nithishvaduganathan07@gmail.com>
2025-12-22 19:46:10 +0530

    fav icon change

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,8 +1,8 @@
 export const personalData = {
-  name: "NITHISH",
+  name: "N",
   profile: '/nithish.jpg',
   designation: "Python & AI Developer",
-  description: "My name is NITHISH. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on python and I love to do most of the things using python. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "My name is Nithish vaduganathan. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on python and I love to do most of the things using python. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'nithishvaduganathan07@gmail.com',
   phone: '+91 8531977455',
   address: 'Thiruvarur,Tamil nadu,india - 609503',
