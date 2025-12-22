700471b24b135fcc216fa65d0345320c13b79fd8
nithishvaduganathan <nithishvaduganathan07@gmail.com>
2025-12-22 19:54:08 +0530

    profile image change

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
-  name: "N",
-  profile: '/nithish.jpg',
+  name: "Nithish Vaduganathan",
+  profile: '/image2.jpg',
   designation: "Python & AI Developer",
   description: "My name is Nithish vaduganathan. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on python and I love to do most of the things using python. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'nithishvaduganathan07@gmail.com',
