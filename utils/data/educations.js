bb652cac3f8dea5b4bb81783e5249375b9a4d951
nithish vaduganathan <143280249+nithishvaduganathan@users.noreply.github.com>
2026-06-29 18:02:44 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - 2024",
+    duration: "2022 - 2026",
     institution: "Sir issac newton college of engineering and technology",
   },
   {
     id: 2,
     title: "Higher Secondary Education",
-    duration: "2018 - 2020",
+    duration: "2020 - 2022",
     institution: "Government Higher Secondary School Poonthottam",
   },
 /*   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
+    duration: "200 - 2018",
     institution: "Baitus Saif Islamia Madrasah",
   } */
 ]
